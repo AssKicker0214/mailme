f309586500e6fca13dd1e0cce4b5c895e8f711c8
njuqhb <121584565@qq.com>
2017-02-12 14:50:00 +0800

    check 500, checkloginbg.php

--- controller/CheckLoginBG.php
+++ controller/CheckLoginBG.php
@@ -5,12 +5,12 @@
  * Date: 2015/12/14
  * Time: 16:57
  */
-echo $_SERVER['DOCUMENT_ROOT'];
-//include_once $_SERVER['DOCUMENT_ROOT']."/model/UserInfo.php";
-//$userid = $_GET['userid'];
-//$psw = $_GET['psw'];
-//$city = $_GET['city'];
-//
+//echo $_SERVER['DOCUMENT_ROOT'];
+include_once $_SERVER['DOCUMENT_ROOT']."/model/UserInfo.php";
+$userid = $_GET['userid'];
+$psw = $_GET['psw'];
+$city = $_GET['city'];
+echo $userid." ".$psw." ".$city;
 //$permit = checkLogin($userid, $psw);
 //
 //if($permit){
