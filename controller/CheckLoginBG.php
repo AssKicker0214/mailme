f180e4e38efbc93a40723ba495f24616047b5a08
njuqhb <121584565@qq.com>
2017-02-12 14:42:53 +0800

    path

--- controller/CheckLoginBG.php
+++ controller/CheckLoginBG.php
@@ -5,15 +5,16 @@
  * Date: 2015/12/14
  * Time: 16:57
  */
-include_once $_SERVER['DOCUMENT_ROOT']."/model/UserInfo.php";
-$userid = $_GET['userid'];
-$psw = $_GET['psw'];
-$city = $_GET['city'];
-
-$permit = checkLogin($userid, $psw);
-
-if($permit){
-    setcookie('userid',$userid,0,'/');
-
-}
-echo $permit,'<br/>µÇÂ½µØµã:',$city;
+echo $_SERVER['DOCUMENT_ROOT'];
+//include_once $_SERVER['DOCUMENT_ROOT']."/model/UserInfo.php";
+//$userid = $_GET['userid'];
+//$psw = $_GET['psw'];
+//$city = $_GET['city'];
+//
+//$permit = checkLogin($userid, $psw);
+//
+//if($permit){
+//    setcookie('userid',$userid,0,'/');
+//
+//}
+//echo $permit,'<br/>ç™»é™†åœ°ç‚¹:',$city;
