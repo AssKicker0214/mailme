30e9be221219f27cea6a1d0762df2e047b86d557
njuqhb <121584565@qq.com>
2017-02-12 14:53:13 +0800

    check 500, checkloginbg.php

--- controller/CheckLoginBG.php
+++ controller/CheckLoginBG.php
@@ -10,8 +10,9 @@ include_once $_SERVER['DOCUMENT_ROOT']."/model/UserInfo.php";
 $userid = $_GET['userid'];
 $psw = $_GET['psw'];
 $city = $_GET['city'];
-echo $userid." ".$psw." ".$city;
-//$permit = checkLogin($userid, $psw);
+//echo $userid." ".$psw." ".$city;
+$permit = checkLogin($userid, $psw);
+echo $permit;
 //
 //if($permit){
 //    setcookie('userid',$userid,0,'/');
